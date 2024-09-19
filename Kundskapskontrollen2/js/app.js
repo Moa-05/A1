10695cd119150a098ef454ecc44f390b5ed764da
Moa-05 <moaber654@ihmeducation.se>
2024-09-19 18:34:20 +0200

    Button

--- Kundskapskontrollen2/js/app.js
+++ Kundskapskontrollen2/js/app.js
@@ -1,63 +1,27 @@
-// Document Elements
+let wins = 1;
+let losses = 1;
+let draws = 1;
 
-// Looks for "text0". Assigns that to the variable called textField0.
-const textField0 = document.getElementById("text0");
+document.getElementById("button0").addEventListener("click", () => {
+  const playerRoll = Math.floor(Math.random() * 6) + 1;
+  const aiRoll = Math.floor(Math.random() * 6) + 1;
 
-// VARIABLES
+  document.getElementById("playerResult").textContent = `playerRoll: ${playerRoll}`;
+  document.getElementById("aiResult").textContent = `aiRoll: ${aiRoll}`;
 
+  let result = "";
 
-// Buttons
-const increaseScoreButton = document.getElementById("button0");
-let button0 = document.getElementById("button0");
-
-
-// Internal Variables
-let score = 0;
-
-// Texts
-let playerRollText = document.getElementById("playerRollText");
-
-// Process (What is going to happen in what sequence?)
-increaseScoreButton.addEventListener("click", () => {
-  increaseScoreByOne();
-  updateScoreText();
-  checkScoreForSeven();
-
-// Data
-  let playerRoll = 0;
-
-// PROCESSES
-  button0.addEventListener("click", function() {
-    getRandomNumberOneToSixForPlayer();
-    showPlayerRollResult();
-  });
-
-
-// Controllers
-  function increaseScoreByOne() {
-    score++;
+  if (playerRoll > aiRoll) {
+    result = "YOU WON!!";
+    wins++;
+  } else if (playerRoll < aiRoll) {
+    result = "YOU LOST!";
+    losses++;
+  } else {
+    result = "DRAW!";
+    draws++;
   }
 
-  function checkScoreForSeven() {
-    if (score >= 7) {
-      changeScoreTextColorToGreen();
-    }
-  }
-
-// View
-  function updateScoreText() {
-    textField0.innerHTML = "Your score is: " + score;
-
-// CONTROLLERS
-    function getRandomNumberOneToSixForPlayer() {
-      playerRoll = Math.floor(Math.random() * 6) + 1;
-    }
-
-    function changeScoreTextColorToGreen() {
-      textField0.style.color = "green";
-    }
-
-// VIEWS
-    function showPlayerRollResult() {
-      playerRollText.innerHTML = playerRoll;
-    }
+  document.getElementById("gameResult").textContent = result;
+  document.getElementById("score").textContent = `Win = ${wins} | Lost = ${losses} | Draw = ${draws}`;
+});
